fb6e4c000d8c8065873ddef83a6fba59ec999026
Nigel Purves <nigel.purves@googlemail.com>
2014-04-06 13:05:16 +0100

    setting up app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MusicMap::Application.config.secret_token = 'c2a01fbb0e61c3daa6de8449d802afab4b4edf72a9160a62d64ea0f8ab0ba56b08c9135763834bc6ea50ba5336340a9d16811b0b3dc4f63ad4b3777da82b39dd'
